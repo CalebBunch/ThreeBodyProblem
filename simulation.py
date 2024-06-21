718ea866b52b9ce1b1405167463ce6a9bb33d913
Luke Jennings <luke@lawn-128-61-54-30.lawn.gatech.edu>
2024-06-21 17:27:11 -0400

    Changed indentations

--- simulation.py
+++ simulation.py
@@ -1,82 +1,81 @@
 import tkinter as tk
-import time
-import turtle
+import turtle as t
 
 G = 6.674 * 10e-11
 
 
 class Planet:
 
-  def __init__(self, pos: list[float], vel: list[float], acc: list[float], mass: float):
-    self._pos = pos
-    self._vel = vel
-    self._acc = acc
-    self._mass = mass
+    def __init__(self, pos: list[float], vel: list[float], acc: list[float], mass: float):
+        self._pos = pos
+        self._vel = vel
+        self._acc = acc
+        self._mass = mass
 
 # Position
 
-  @property
-  def pos(self) -> list[float]:
-    return self._pos
+    @property
+    def pos(self) -> list[float]:
+        return self._pos
 
-  @pos.setter
-  def pos(self, newpos: list[float]) -> None:
-    self._pos = newpos
+    @pos.setter
+    def pos(self, newpos: list[float]) -> None:
+        self._pos = newpos
 
 # Velocity
 
-  @property
-  def vel(self) -> list[float]:
-    return self._vel
+    @property
+    def vel(self) -> list[float]:
+        return self._vel
 
-  @vel.setter
-  def vel(self, newvel: list[float]) -> None:
-    self._vel = newvel
+    @vel.setter
+    def vel(self, newvel: list[float]) -> None:
+        self._vel = newvel
 
 # Acceleration
 
-  @property
-  def acc(self) -> list[float]:
-    return self._acc
+    @property
+    def acc(self) -> list[float]:
+        return self._acc
 
-  @acc.setter
-  def acc(self, newacc: list[float]) -> None:
-    self._acc = newacc
+    @acc.setter
+    def acc(self, newacc: list[float]) -> None:
+        self._acc = newacc
 
 
 # Mass
 
-  @property
-  def mass(self) -> float:
-    return self._mass
+    @property
+    def mass(self) -> float:
+        return self._mass
 
-  def calculate_distance(self, other: "Planet", index: int) -> float:
-    return ((self.pos[index] - other.pos[index])**2)**0.5
+    def calculate_distance(self, other: "Planet", index: int) -> float:
+        return ((self.pos[index] - other.pos[index])**2)**0.5
 
-  def gravitational_force(self, other: "Planet") -> list[float]:
-    forces = []
-    for idx in range(len(self.pos)):
-      forces.append(G * self.mass * other.mass /
-                    (self.calculate_distance(other, idx) + 1e-9)**2)
-    return forces
+    def gravitational_force(self, other: "Planet") -> list[float]:
+        forces = []
+        for idx in range(len(self.pos)):
+            forces.append(G * self.mass * other.mass /
+                (self.calculate_distance(other, idx) + 1e-9)**2)
+        return forces
 
 
 def update(planets: list[Planet], dt: float):
-  for i in range(len(planets)):
-    for j in range(len(planets)):
-      if i != j:
-        forces = planets[i].gravitational_force(planets[j])
-        planets[i].acc = [a / planets[i].mass for a in forces]
-    planets[i].vel = [
-        planets[i].vel[0] + dt * planets[i].acc[0],
-        planets[i].vel[1] + dt * planets[i].acc[1],
-        planets[i].vel[2] + dt * planets[i].acc[2]
-    ]
-    planets[i].pos = [
-        planets[i].pos[0] + dt * planets[i].vel[0],
-        planets[i].pos[1] + dt * planets[i].vel[1],
-        planets[i].pos[2] + dt * planets[i].vel[2]
-    ]
+    for i in range(len(planets)):
+        for j in range(len(planets)):
+            if i != j:
+                forces = planets[i].gravitational_force(planets[j])
+                planets[i].acc = [a / planets[i].mass for a in forces]
+        planets[i].vel = [
+            planets[i].vel[0] + dt * planets[i].acc[0],
+            planets[i].vel[1] + dt * planets[i].acc[1],
+            planets[i].vel[2] + dt * planets[i].acc[2]
+        ]
+        planets[i].pos = [
+            planets[i].pos[0] + dt * planets[i].vel[0],
+            planets[i].pos[1] + dt * planets[i].vel[1],
+            planets[i].pos[2] + dt * planets[i].vel[2]
+        ]
 
 
 def main() -> None:
