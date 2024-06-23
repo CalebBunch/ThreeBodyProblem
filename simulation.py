b0f8409d5c2dc2108aea15ccdfa44e10b6d5274c
Luke Jennings <luke@localhost.localdomain>
2024-06-23 12:34:27 -0400

    cleanup and notes

--- simulation.py
+++ simulation.py
@@ -4,9 +4,11 @@ import time
 from threading import Thread
 import math
 
+#TODO tkinter initial positions/color customization, dt?
 
 G = 6.674 * 10e-11
 
+sig_stretch = 0.01
 
 class Planet:
     def __init__(self, pos: list[float], vel: list[float], mass: float, canvas: tk.Canvas) -> None:
@@ -31,7 +33,8 @@ class Planet:
         self._pos = newpos
         self._turtle.goto(newpos[0], newpos[1])
 
-        sigbase = (lambda z: 1/(1+math.exp(-0.01*z)))(newpos[2]) #change 0.01 for diff colors??
+        global sig_stretch # make changeable with tkinter gui. must be between 0 and 1
+        sigbase = (lambda z: 1/(1+math.exp(-sig_stretch*z)))(newpos[2])
         #g_val = (lambda z: (z/(1+abs(z))+1)/2)(newpos[2])
         #b_val = (lambda z: (math.tanh(z)+1)/2)(newpos[2])
 
@@ -77,19 +80,20 @@ class Planet:
 
     def gravitational_force(self, other: "Planet") -> list[float]:
         dist_vector = self.calculate_distance_vector(other)
-        theta = math.atan(dist_vector[1] / (dist_vector[0] + 1e-9)) # angle of force in x y plane
+        theta = math.atan(dist_vector[1] / (dist_vector[0] + 1e-9)) # angle of force vector in x y plane
  
         if vector_magnitude(dist_vector) == 0:
             grav_mag = 0
         else:
             grav_mag = (G * self._mass * other.mass) / (vector_magnitude(dist_vector)**2)
        
-        phi = math.asin(dist_vector[2] / (vector_magnitude(dist_vector) + 1e-9))
+        phi = math.asin(dist_vector[2] / (vector_magnitude(dist_vector) + 1e-9)) # angle of force vector in x z plane
 
         grav_x = grav_mag*math.cos(theta)*math.cos(phi)
         grav_y = grav_mag*math.sin(theta)*math.cos(phi)
         grav_z = grav_mag*math.sin(phi)
 
+        # clarify direction of components
         if self._pos[0] > other.pos[0]:
             grav_x = -abs(grav_x)
         if self._pos[0] < other.pos[0]:
@@ -103,11 +107,10 @@ class Planet:
         if self._pos[2] < other.pos[2]:
             grav_z = abs(grav_z)
 
+        # collision detection
         if vector_magnitude(dist_vector) < 5:
             grav_x, grav_y, grav_z = 0, 0, 0
         
-        #print(grav_x, grav_y)
-        #print(dist_vector)
         return([grav_x, grav_y, grav_z])
 
 def update(planets: list[Planet], dt: float) -> None:
@@ -116,7 +119,7 @@ def update(planets: list[Planet], dt: float) -> None:
         for j in range(len(planets)):
             if i != j:
                 #print(len(planets[j].pos), planets[j].pos)
-                print(planets[j].pos[2], j)
+                #print(planets[j].pos[2], j)
                 forces = list(map(sum, zip(forces, planets[i].gravitational_force(planets[j]))))
         planets[i].acc = [f / planets[i].mass for f in forces]
 
@@ -126,6 +129,7 @@ def update(planets: list[Planet], dt: float) -> None:
             planets[i].vel[2] + dt * planets[i].acc[2]
         ]
 
+    # update pos AFTER acc and vel because they are dependant on pos
     for i in range(len(planets)):
         planets[i].pos = [
             planets[i].pos[0] + dt * planets[i].vel[0],
@@ -137,14 +141,14 @@ def vector_magnitude(v: list[float]) -> float:
     return sum([s * s for s in v]) ** 0.5
 
 def run(planets: list[Planet]) -> None:
-    #t = time.clock_gettime(time.CLOCK_MONOTONIC)
-    i = 0
+    #t = time.clock_gettime(time.CLOCK_MONOTONIC)*1e-20
+    #i = 0
     while True:
-        #dt = time.clock_gettime(time.CLOCK_MONOTONIC) - t  # in seconds
+        #dt = time.clock_gettime(time.CLOCK_MONOTONIC)*1e-20 - t  # in seconds
         
         dt = 0.01
         update(planets, dt)
-        #t = time.clock_gettime(time.CLOCK_MONOTONIC)
+        #t = time.clock_gettime(time.CLOCK_MONOTONIC)*1e-20
         #i += 1
         #if i > 1:
         #    break
@@ -159,8 +163,8 @@ def main() -> None:
     canvas.pack()
     
     planet1 = Planet(pos=[-100, 0, 0], vel=[34.71128135672417, 53.2726851767674, 0], mass=1e15, canvas=canvas)
-    planet2 = Planet(pos=[0, 0, 0], vel=[0, 0, 19], mass=1e15, canvas=canvas)
-    planet3 = Planet(pos=[100, 0, 0], vel=[0, 0, 0], mass=1e15, canvas=canvas)
+    planet2 = Planet(pos=[0, 10, 50], vel=[0, 0, 19], mass=1.5e15, canvas=canvas)
+    planet3 = Planet(pos=[100, 0, 0], vel=[-30, -50, 0], mass=1e15, canvas=canvas)
 
     #planet1 = Planet(pos=[-300, 300, 50], vel=[10, 10, -10], mass=1e15, canvas=canvas)
     #planet2 = Planet(pos=[0, 100, 30], vel=[0, 0, 0], mass=1e15, canvas=canvas)
